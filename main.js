e65b359f0ec0af7b3b41970f3d71e8209d66c7c8
Roman Matsypula <ukrmatsypula@gmail.com>
2021-10-26 22:56:03 +0300

    primitive data in js

---
+++ main.js
@@ -0,0 +1,14 @@
+// numbers
+console.log(2021);
+console.log(20 * "hello");
+console.log(1 / 0);
+// strings
+console.log("Hello", "Hello", `Hello`);
+// boolean
+console.log(true, false);
+// null
+console.log(null); //имеет единственное значение
+// undefined
+console.log(undefined); // также единственное значание
+
+// Обьекты
