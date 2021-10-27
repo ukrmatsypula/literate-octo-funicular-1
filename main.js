5a09ae41a44cb3f04974a4ab2babd310109a450b
Roman Matsypula <ukrmatsypula@gmail.com>
2021-10-27 20:14:02 +0300

    added typeof check to

--- main.js
+++ main.js
@@ -12,3 +12,28 @@ console.log(null); //имеет единственное значение
 console.log(undefined); // также единственное значание
 
 // Обьекты
+console.log({ name: "Denis", age: 30 });
+// преобразование в типы данных
+let value;
+value = String(10);
+value = String(10 + 40);
+value = (40).toString();
+value = 30 + '';
+value = 30 - 'sad'; // NaN
+value = true;
+value = String([1, 2, 3]); // array to string
+value = String({ name: "Denis" }); // object to string
+value = 30 - '5' // number 25
+value = true + 10; // 11
+value = true + '10'
+value = false + undefined // NaN
+//String to number
+value = Number('23') // NaN
+value = Number(true) // 1
+value = Number(null) // 0
+value = Number('null') // NaN
+value = Number([1, 2, 3]) // NaN
+
+
+console.log(value);
+console.log(typeof value);
