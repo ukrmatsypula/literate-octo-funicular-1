e8b259f95c6eacd0288eb088e3d481c5e52e42f8
Roman Matsypula <ukrmatsypula@gmail.com>
2021-11-06 23:39:23 +0200

    added event bubble

--- main.js
+++ main.js
@@ -906,3 +906,27 @@ let users = [
 //     console.log("button clicked");
 //   }
 // });
+
+const btn = document.querySelector('.btn')
+const wrap = document.querySelector('.wrap')
+
+btn.addEventListener('click', (e) => {
+  // e.stopPropagation()
+  console.log('click btn');
+})
+
+wrap.addEventListener('click', (e) => {
+  console.log('click wrap');
+})
+
+document.body.addEventListener('click', (e) => {
+  console.log('click body')
+})
+
+wrap.addEventListener('click', (e) => {
+  console.log('click wrap');
+}, true)
+
+document.body.addEventListener('click', (e) => {
+  console.log('click body')
+}, true)
