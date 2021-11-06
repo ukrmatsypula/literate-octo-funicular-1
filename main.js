2227dc317c8f1069b8c85a59bacd209fe1cba794
Roman Matsypula <ukrmatsypula@gmail.com>
2021-11-06 23:26:52 +0200

    added events

--- main.js
+++ main.js
@@ -889,20 +889,20 @@ let users = [
 //   console.log("click", e);
 // };
 
-const container = document.querySelector(".container");
-const btn = document.querySelector("button");
+// const container = document.querySelector(".container");
+// const btn = document.querySelector("button");
 
-btn.addEventListener("click", (e) => {
-  const div = document.createElement("div");
-  const nestedBtn = document.createElement("button");
-  nestedBtn.textContent = "button";
-  div.textContent = Math.random();
-  div.appendChild(nestedBtn);
-  container.appendChild(div);
-});
+// btn.addEventListener("click", (e) => {
+//   const div = document.createElement("div");
+//   const nestedBtn = document.createElement("button");
+//   nestedBtn.textContent = "button";
+//   div.textContent = Math.random();
+//   div.appendChild(nestedBtn);
+//   container.appendChild(div);
+// });
 
-container.addEventListener("click", (e) => {
-  if (e.target.tagName === "BUTTON") {
-    console.log("button clicked");
-  }
-});
+// container.addEventListener("click", (e) => {
+//   if (e.target.tagName === "BUTTON") {
+//     console.log("button clicked");
+//   }
+// });
