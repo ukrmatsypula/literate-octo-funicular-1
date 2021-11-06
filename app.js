e47458c7afec934e1079e3affbc69a58ed176fad
Roman Matsypula <ukrmatsypula@gmail.com>
2021-11-07 00:42:01 +0200

    rendered li > span > button > article inside li and add it to fragment

--- app.js
+++ app.js
@@ -42,8 +42,37 @@ const tasks = [
     }
 
     const fragment = document.createDocumentFragment();
-    Object.values(tasksList).forEach(task => {
-      console.log(task)
+    Object.values(tasksList).forEach((task) => {
+      const li = listItemTemplate(task);
+      fragment.appendChild(li)
     });
+  }
+
+  function listItemTemplate({ _id, title, body } = {}) {
+    const li = document.createElement("li");
+    li.classList.add(
+      "list-group-item",
+      "d-flex",
+      "align-items-center",
+      "flex-wrap",
+      "mt-2"
+    );
+    const span = document.createElement("span");
+    span.textContent = title;
+    span.style.fontWeight = "bold";
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Delete task";
+    deleteBtn.classList.add("btn", "btn-danger", "ml-auto", "delete-btn");
+
+    const article = document.createElement("p");
+    article.textContent = body;
+    article.classList.add("mt-2", "w-100");
+
+    li.appendChild(span);
+    li.appendChild(deleteBtn);
+    li.appendChild(article);
+
+    return li;
   }
 })(tasks);
